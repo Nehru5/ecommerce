e35b2e861414ff62fe4b47a0a46669c694ad1dd1
Nehru raja <nehruraja9485@gmail.com>
2026-03-26 13:55:38 +0530

    Day 1

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <div className="nav">
+        <div className="left">
+          <h2>Cartify</h2>
+        </div>
+        <div className="right">
+          <h2>Products</h2>
+          <h2>Cart</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Nav
---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Nav from "../components/Nav"
+import Footer from '../components/Footer'
+const Home = () => {
+  return (
+    <>
+    <Nav/>
+      <div className="hero-section">
+        <h2>Welcome to Cartify</h2>
+        <button>Shop now</button>
+      </div>
+    <center><h1>Products</h1></center>
+      <div className="products">
+        <img src="https://www.tpupolymer.com/js/htmledit/kindeditor/attached/20220729/20220729181358_47088.jpg" alt="" />
+        <img src="https://mzfoodtest.com/wp-content/uploads/2022/06/dairy-products.jpg" alt="" />
+        <img src="https://www.news-medical.net/images/Article_Images/ImageForArticle_14459_1718253799749257.jpg" alt="" />
+      </div>
+
+      <div className="products">
+        <img src="https://www.tpupolymer.com/js/htmledit/kindeditor/attached/20220729/20220729181358_47088.jpg" alt="" />
+        <img src="https://mzfoodtest.com/wp-content/uploads/2022/06/dairy-products.jpg" alt="" />
+        <img src="https://www.news-medical.net/images/Article_Images/ImageForArticle_14459_1718253799749257.jpg" alt="" />
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
